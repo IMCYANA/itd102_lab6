fb78511d1d8f51a55badb2264444d7cc9b3c7ac2
IMCYANA <yardkorncha@gmail.com>
2024-12-13 12:32:56 +0700

    lab7

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
+const db = require('./app/models');
+db.sequelize.sync({force: false}).then(()=>{
+    console.log("DB Connected");
+});
+
 // app.get('/', (req, res) => { 
 //     res.send('Home');
 // });
